c7802b38cb69ceff7ac1af9b67d3815cbf631f90
Gaurav Asode <gauravasode5@gmail.com>
2023-10-22 19:18:08 +0530

    folder structure

---
+++ Backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const io = require("socket.io");
+const { connection } = require("./config/db");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const PORT = process.env.PORT || 3000 ;
+
+app.listen(PORT, async()=>{
+    try {
+        await connection;
+        console.log(`Server is running on port ${PORT}`);
+        console.log("Connected to DataBase")
+    } catch (error) {
+        console.log(error);
+        console.log("Cannot connect to DataBase")
+    }
+});
